e935c85376dd87b03028b9ef7c80d96ad5bb0724
jszh <969883825@qq.com>
2019-02-20 14:46:20 +0800

    🎉🎉

--- z_view/view/page2_Weather/weather.js
+++ z_view/view/page2_Weather/weather.js
@@ -48,7 +48,7 @@ export default class weather extends Component {
     onBack = () =>{
         pop();
         return true;
-    }
+    };
     setModalVisible(visible) {
         this.setState({modalVisible: visible});
     }
